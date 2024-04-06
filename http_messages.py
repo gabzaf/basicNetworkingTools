69246000c87441135eff6e3595a0ebe794f3a232
gab Deb <gabriel.affonsoo@gmail.com>
2024-04-06 17:52:34 +0100

    A script to capture http request and response messages

---
+++ http_messages.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+r = requests.get('https://www.google.com')
+request_object = r.request.headers
+response_object = r.headers
+
+print("Request Header:")
+for key, value in request_object.items():
+    print(f"{key}: {value}")
+
+print("\nResponse Header:")
+for key, value in response_object.items():
+    print(f"{key}: {value}")
