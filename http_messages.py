6da6ef6e29ad8847bc855f88d5fcf53ea1896205
gab Deb <gabrielaffonsoo@gmail.com>
2024-04-07 13:25:30 +0100

    Add some info on print

--- http_messages.py
+++ http_messages.py
@@ -1,13 +1,14 @@
 import requests
 
-r = requests.get('https://www.google.com')
-request_object = r.request.headers
-response_object = r.headers
+r = requests.get('http://www.google.com')
+request_header = r.request.headers
+response_header = r.headers
 
-print("Request Header:")
-for key, value in request_object.items():
+print("Request Message:")
+print("Method:",r.request.method)
+for key, value in request_header.items():
     print(f"{key}: {value}")
-
-print("\nResponse Header:")
-for key, value in response_object.items():
+print("\nResponse Message:")
+print("Status Code:", r.status_code)
+for key, value in response_header.items():
     print(f"{key}: {value}")
